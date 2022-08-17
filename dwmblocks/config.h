cf2b01a34a520ecfe89a74e52266a2d0d3f5f308
reftch <j.developer@hotmail.com>
2022-08-17 13:34:33 +0200

    Updated scripts

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -8,7 +8,7 @@ const Block blocks[] = {
   BLOCK("sb-memory",      10,   23),
   BLOCK("sb-cpu-usage",   1,    25),
   BLOCK("sb-cpu",         10,   26),
-  BLOCK("sb-internet",    5,    27),
-	BLOCK("sb-battery",     5,    28),
-  BLOCK("sb-date",        1,    29)
+  BLOCK("sb-internet",    20,    27),
+	BLOCK("sb-battery",     20,    28),
+  BLOCK("sb-date",        60,    29)
 };
