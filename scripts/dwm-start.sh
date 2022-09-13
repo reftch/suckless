d9413b7c9fadd6d4cfe5ce097c261eab989e8a72
reftch <j.developer@hotmail.com>
2022-09-13 22:12:32 +0200

    Adjusted scripts

--- scripts/dwm-start.sh
+++ scripts/dwm-start.sh
@@ -5,7 +5,7 @@ xset s 3600 3600
 # Notification-daemon 
 dunst &
 # Compositor
-picom &
+picom --backend glx --glx-no-stencil --vsync --corner-radius 6 &
 # Set wallpaper
 feh --bg-scale /usr/share/backgrounds/background_3.jpg &
 # DWM status bar
